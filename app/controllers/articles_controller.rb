73e8284c4609ba77f5db10535fbaaf64ab6334bf
Rails Udemy Course <van@sigmaits.com>
2016-12-30 18:27:05 +0000

    Fix create, add flash and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,19 @@ class ArticlesController < ApplicationController
         
     end
     
+    def show
+       @article = Article.find(params[:id]) 
+    end
+    
     def create
-      #render plain: params[:article].inspect  
       @article = Article.new(article_params)
-      @article.save
-      redirect_to articles_show(@article)
+      if @article.save
+          flash[:notice] = "Article was successfully created"
+            #do something
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
     end
     
     private
