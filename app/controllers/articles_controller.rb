d9b943cff53fe35d48713b471023591d9cbb51e0
Rails Udemy Course <van@sigmaits.com>
2017-01-03 00:24:16 +0000

    User info into Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
     
     def create
       @article = Article.new(article_params)
-      # @article.user = User.first
+      @article.user = User.first
             if @article.save
           flash[:success] = "Article was successfully created"
             #do something
